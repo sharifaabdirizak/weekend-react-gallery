97747dc4f3286fd95ed865e47bae2d020d7ce112
Sharifa Abdirizak <sharifamabdirizak@gmail.com>
2023-09-18 08:06:12 -0500

    added README.md

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -12,6 +12,33 @@ const galleryItems = [
     description: "Photo of the beach in the Carribeans.",
     likes: 0,
   },
+
+  {
+    id: 3,
+    path: "images/africa.jpeg",
+    description: "African culture photo.",
+    likes: 0,
+  },
+
+  {
+    id: 4,
+    path: "images/travel.jpeg",
+    description: "I love to travel and connect with family and friends.",
+    likes: 0,
+  },
+
+  {
+    id: 5,
+    path: "images/greenbaypacker.jpeg",
+    description: "My favorite football team, better the Vikings lol.",
+    likes: 0,
+  },
+  {
+    id: 6,
+    path: "images/food.jpeg",
+    description: "Foods I like to eat that are clean and healthy.",
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
